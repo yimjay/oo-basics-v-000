5576d4eea37273fe0ee349e9514babb270108d2f
Ji Yim <gsooyim@gmail.com>
2018-04-24 22:15:35 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,14 @@
 class Shoe
+  def initialize(brand)
+    @brand
+  end
   
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def cobble
+    puts "shoe has been repaired"
+    @condition: new
+  end
   
   
 end
